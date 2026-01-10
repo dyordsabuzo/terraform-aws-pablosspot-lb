ec64ad09d2f53cdb6ece030fb6fe7e399ef95f4d
Dyords Abuzo <dyordsabuzo@gmail.com>
2026-01-10 19:28:23 +1100

    Add description to output

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,9 @@
 output "lb_listener_arn" {
-  value = aws_lb_listener.listener.arn
+  value       = aws_lb_listener.listener.arn
+  description = "Load balancer listener arn"
 }
 
 output "lb_dns" {
-  value = aws_lb.lb.dns_name
+  value       = aws_lb.lb.dns_name
+  description = "Load balancer dns name"
 }
